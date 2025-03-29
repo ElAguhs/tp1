0e90bd9930e841d87ca89812f2e3881d1e725309
“ElAguhs” <rodriguezagustin589@gmail.com>
2025-03-29 12:22:50 -0300

    valores invertidos

--- tp1_2.c
+++ tp1_2.c
@@ -7,6 +7,8 @@ void cuadradoNumero2(int *p);
 
 void direccionyContenido(int *p);
 
+void invertirValores(int *num1, int *num2);
+
 int main()
 {
 
@@ -22,6 +24,18 @@ int main()
     cuadradoNumero2(&numero);
 
     direccionyContenido(&numero);
+
+    printf("El valor del 1er numero.\n");
+
+    int numero2, numero3;
+
+    scanf("%d", &numero2);
+
+    printf("El valor del 2do numero.\n");
+
+    scanf("%d", &numero3);
+
+    invertirValores(&numero2, &numero3);
 
     return 0;
 }
@@ -43,4 +57,18 @@ void direccionyContenido(int *p)
 {
 
     printf("La direccion de la variable:%p\nEl contenido de la variable %d\n", p, *p);
+}
+
+void invertirValores(int *num1, int *num2)
+{
+
+    printf("Valor del 1er numero:%d\nValor del 2do numero:%d\n", *num1, *num2);
+
+    int numero1 = *num2;
+
+    int numero2 = *num1;
+
+    printf("Valor del 1er numero invertido:%d\nValor del 2do numero invertido:%d\n", numero1, numero2);
+
+
 }
