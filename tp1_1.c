57464e42ac0313bab1038b594f0838237324ae9b
“ElAguhs” <rodriguezagustin589@gmail.com>
2025-03-29 11:34:21 -0300

    .c terminado

--- tp1_1.c
+++ tp1_1.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
 
-printf("Hola mundo");
+    printf("Hola mundo\n");
+
+    int *p, numero = 5;
+
+    p = &numero;
 
 
+
+    printf("Contenido puntero: %d \nDireccion de memoria almacenada por el puntero: %p\nDireccion de memoria de la variable:%p\nTamano de memoria utilizada por la variable numero:%zu", *p, p,&numero, sizeof(numero) );
 }
