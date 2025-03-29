c26606132e97c586018f845c647dd44cda3f9b37
“ElAguhs” <rodriguezagustin589@gmail.com>
2025-03-29 11:36:28 -0300

    ahora si archivo terminado 100%

--- tp1_1.c
+++ tp1_1.c
@@ -11,7 +11,7 @@ int main()
 
 
 
-    printf("Contenido puntero: %d \nDireccion de memoria almacenada por el puntero: %p\nDireccion de memoria de la variable:%p\nTamano de memoria utilizada por la variable numero:%zu", *p, p,&numero, sizeof(numero) );
+    printf("Contenido puntero: %d \nDireccion de memoria almacenada por el puntero: %p\nDireccion de memoria de la variable:%p\nTamano de memoria utilizada por la variable numero:%d\n", *p, p,&numero, sizeof(numero) );
 
     return 0;
 }
